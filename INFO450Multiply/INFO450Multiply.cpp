fd18bab01ddd7c0b504ff4b7ca0f8573d72c815b
Corey Cross <crosscm2@vcu.edu>
2016-09-11 21:39:06 -0400

    homeworkAssignment1

--- INFO450Multiply/INFO450Multiply.cpp
+++ INFO450Multiply/INFO450Multiply.cpp
@@ -2,10 +2,23 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
 int main()
 {
-    return 0;
+	cout << "My Multiplication Table:" << endl
+		<< "   1\t2\t3\t4\t5\t6\t7\t8\t9\t10" << endl
+		<< "" << endl;
+	for (int c = 1; c < 11; c++)
+	{
+		cout << c << "| ";
+		for (int i = 1; i < 11; i++)
+		{
+			cout << i * c << '\t';
+		}
+		cout << endl;
+	}
+	return 0;
 }
-
